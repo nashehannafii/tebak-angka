0bf49339df2bac039bedac8908d4ce5d18d6e0ea
nashehannafii <nashehannafii@gmail.com>
2026-06-10 11:32:57 +0700

    update

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const rooms = {};
 
